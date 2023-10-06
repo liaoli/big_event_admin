177487db340227aa81670023415b10b6a82cb111
liaoli <277218962@qq.com>
2023-10-06 15:59:40 +0800

    net work

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-
+import { useUserStore } from '../stores'
+import { ElMessage } from 'element-plus'
+import router from '@/rounter'
 const baseURL = 'http://big-event-vue-api-t.itheima.net'
 
 const instance = axios.create({
@@ -11,6 +13,10 @@ const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     // TODO 2. 携带token
+    const userStore = useUserStore()
+    if (userStore.token) {
+      config.headers.Authorization = userStore.token
+    }
     return config
   },
   (err) => Promise.reject(err)
@@ -18,14 +24,25 @@ instance.interceptors.request.use(
 
 instance.interceptors.response.use(
   (res) => {
-    // TODO 3. 处理业务失败
-    // TODO 4. 摘取核心响应数据
-    return res
+    // TODO 3. 摘取核心响应数据
+    if (res.data.code === 0) {
+      return res
+    }
+    // TODO 4. 处理业务失败
+    ElMessage({
+      message: res.data.message || '服务异常',
+      type: 'error'
+    })
+    return Promise.reject(res.data)
   },
   (err) => {
     // TODO 5. 处理401错误
+    if (err.response?.status === 401) {
+      router.push('/login')
+    }
     return Promise.reject(err)
   }
 )
 
 export default instance
+export { baseURL }
